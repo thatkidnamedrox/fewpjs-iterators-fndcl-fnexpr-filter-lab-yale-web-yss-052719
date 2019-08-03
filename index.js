e4267034e5619b9bbdb189eac5ef4df589fc94bb
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-08-03 01:38:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(drivers, name) {
+
+  return drivers.filter(d => {
+    console.log(d, name);
+    let dName = d[0].toUpperCase() + d.slice(1);
+    return dName === name
+  })
+}
+
+function fuzzyMatch(drivers, str) {
+  return drivers.filter(d => {
+    return d.startsWith(str)
+  })
+}
+
+function matchName(drivers, str) {
+  return drivers.filter((d) => {
+    return d.name === str
+  })
+}
